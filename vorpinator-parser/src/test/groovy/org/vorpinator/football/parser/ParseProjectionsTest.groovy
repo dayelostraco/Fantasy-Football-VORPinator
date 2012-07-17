6c046e6dac836c1755cd53188ca6dae6d123b714
Dayel Ostraco <dayel.ostraco@gmail.com>
2012-07-16 23:02:54 -0400

    Added Maven Tests Folder

---
+++ vorpinator-parser/src/test/groovy/org/vorpinator/football/parser/ParseProjectionsTest.groovy
@@ -0,0 +1,14 @@
+package org.vorpinator.football.parser
+
+/**
+ * User: Dayel Ostraco
+ * Date: 7/16/12
+ * Time: 11:01 PM
+ */
+class ParseProjectionsTest extends GroovyTestCase {
+
+    void testSomething() {
+        assert 1 == 1
+        assert 2 + 2 == 4 : "We're in trouble, arithmetic is broken"
+    }
+}
